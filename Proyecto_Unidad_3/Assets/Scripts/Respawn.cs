f5f830a3783c160e140e078c2293bc5a302cbf4e
DanielMoreno17 <luis17moreno117@gmail.com>
2024-03-19 08:49:19 -0600

    Proyecto de unidad 3

---
+++ Proyecto_Unidad_3/Assets/Scripts/Respawn.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject spawn_point;
+    [SerializeField]
+    float altura;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = transform.position.y;
+        if (y < altura)
+        {
+            transform.position = spawn_point.transform.position;
+        }
+    }
+}
